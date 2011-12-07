fbeb33ca139df6853d12745b1a9d3ae68197d971
Zach Pfeffer <zach.pfeffer@linaro.org>
2011-12-07 16:06:23 -0600

    gralloc_ump: initial commit
    From:
    
    http://www.malideveloper.com/developer-resources/drivers/open-source-mali-gpus-android-gralloc-module.php
    
    This is an Android Gralloc module which allocates memory from the
    Unified Memory Profider (UMP) device driver. The Gralloc module is
    used by Android's composition engine in order to allocate and manage
    memory which is suitable for 2D and 3D graphics usage. The Mali driver
    stack is able to use UMP memory directly, resulting in zero-copy
    operations when compositing.
    
    Signed-off-by: Angus Ainslie <angus.ainslie@linaro.org>
    Signed-off-by: Zach Pfeffer <zach.pfeffer@linaro.org>

---
+++ Android.mk
@@ -0,0 +1,42 @@
+# 
+# Copyright (C) 2010 ARM Limited. All rights reserved.
+# 
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(TARGET_BOARD_PLATFORM),origen)
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation, not prelinked and stored in
+# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libMali libGLESv1_CM libUMP
+LOCAL_CFLAGS := -fpermissive
+
+# Include the UMP header files
+LOCAL_C_INCLUDES := hardware/samsung/origen/ump/include
+
+LOCAL_SRC_FILES := \
+	gralloc_module.cpp \
+	alloc_device.cpp \
+	framebuffer_device.cpp
+
+LOCAL_MODULE := gralloc.origen
+LOCAL_MODULE_TAGS := eng
+LOCAL_CFLAGS:= -DLOG_TAG=\"gralloc_ump\" -DGRALLOC_32_BITS -DSTANDARD_LINUX_SCREEN -fpermissive
+#LOCAL_CFLAGS+= -DMALI_VSYNC_EVENT_REPORT_ENABLE
+include $(BUILD_SHARED_LIBRARY)
+endif
